8d5b593bb47b61cd24ecdea9eff9c61735c70434
Ben <benparker03@gmail.com>
2022-07-14 11:45:08 +0100

    fixed input box sizing and changed type to tel to try to force number pad input on mobile

--- main.js
+++ main.js
@@ -1,117 +1,123 @@
-import { depthFirstSolver, isValidBoard, isValidEntry } from './modules/solver.js';
-import { boards } from './modules/boards.js';
+import {
+  depthFirstSolver,
+  isValidBoard,
+  isValidEntry,
+} from "./modules/solver.js";
+import { boards } from "./modules/boards.js";
 
 function generateBoard(boards) {
   const board = boards[Math.floor(Math.random() * boards.length)];
 
   board.forEach((el, i) => {
     const element = document.getElementById(String(i + 1));
-    element.parentElement.classList.remove('invalid');
+    element.parentElement.classList.remove("invalid");
     if (el === 0) {
-      element.value = ''; 
+      element.value = "";
       element.readOnly = false;
-      element.classList.add('free');
+      element.classList.add("free");
     } else {
       element.value = el;
       element.readOnly = true;
-      element.classList.remove('free');
+      element.classList.remove("free");
     }
   });
 }
 
 function resetBoard() {
-  const inputs = document.querySelectorAll('.sudoku-board-cell>input');
+  const inputs = document.querySelectorAll(".sudoku-board-cell>input");
 
-  inputs.forEach(input => {
+  inputs.forEach((input) => {
     input.parentElement.classList.remove("added");
-    input.parentElement.classList.remove('invalid');
+    input.parentElement.classList.remove("invalid");
     if (input.readOnly !== true) {
-      input.value = '';
+      input.value = "";
     }
   });
 }
 
 function getCurrentBoard() {
   const board = [];
-  const inputs = document.querySelectorAll('.sudoku-board-cell>input');
+  const inputs = document.querySelectorAll(".sudoku-board-cell>input");
 
-  inputs.forEach(input => {
+  inputs.forEach((input) => {
     const val = input.value;
-    if (val === '') board.push(0);
+    if (val === "") board.push(0);
     else board.push(parseInt(val));
   });
 
   return board;
 }
 
-window.addEventListener('DOMContentLoaded', (e) => {
+window.addEventListener("DOMContentLoaded", (e) => {
   // add html for board
-  const boardElement = document.getElementById('sudoku-board');
+  const boardElement = document.getElementById("sudoku-board");
   for (let i = 1; i <= 81; i++) {
     boardElement.innerHTML += `
       <div class="sudoku-board-cell">
-        <input type="text" pattern="^[1-9]$" id="${i}" maxlength="1">
+        <input type="tel" pattern="$[1-9]^" id="${i}" maxlength="1">
       </div>
-    `
+    `;
   }
-  
+
   // generate first board;
   generateBoard(boards);
 
-  const inputs = document.querySelectorAll('.sudoku-board-cell>input.free');
-  inputs.forEach(input => {
+  const inputs = document.querySelectorAll(".sudoku-board-cell>input.free");
+  inputs.forEach((input) => {
     // check validity of input on focusout and remove if not valid
-    input.addEventListener('focusout', (e) => {
-      if (input.value === '') {
-        input.parentElement.classList.remove('invalid');
+    input.addEventListener("focusout", (e) => {
+      if (input.value === "") {
+        input.parentElement.classList.remove("invalid");
         return;
       }
 
       if (!input.validity.valid) {
-        input.value = '';
-        input.parentElement.classList.remove('invalid');
+        input.value = "";
+        input.parentElement.classList.remove("invalid");
         return;
       }
     });
 
     // check validity of entry on keyup and highlight red if not valid
-    input.addEventListener('keyup', (e) => {
+    input.addEventListener("keyup", (e) => {
       // check if number 1-9 else return
-      const regex = new RegExp('^[1-9]$');
+      const regex = new RegExp("^[1-9]$");
       if (!regex.test(input.value)) return;
 
       const board = getCurrentBoard();
       const valid = isValidEntry(board, input.id - 1);
 
       if (!valid) {
-        input.parentElement.classList.add('invalid');
+        input.parentElement.classList.add("invalid");
       } else {
-        input.parentElement.classList.remove('invalid');
+        input.parentElement.classList.remove("invalid");
       }
     });
   });
 
-  // generate button generates a new board at random 
-  const generateButton = document.getElementById('button-generate');
-  generateButton.addEventListener('click', (e) => {
+  // generate button generates a new board at random
+  const generateButton = document.getElementById("button-generate");
+  generateButton.addEventListener("click", (e) => {
     generateBoard(boards);
   });
 
   // reset button resets board to original state
-  const resetButton = document.getElementById('button-reset');
-  resetButton.addEventListener('click', (e) => {
+  const resetButton = document.getElementById("button-reset");
+  resetButton.addEventListener("click", (e) => {
     resetBoard();
   });
 
   // solve button checks validity of board and attempts to solve if valid
-  const solveButton = document.getElementById('button-solve');
-  solveButton.addEventListener('click', (e) => {
+  const solveButton = document.getElementById("button-solve");
+  solveButton.addEventListener("click", (e) => {
     // check for invalid entries before attempting to solve
     const board = getCurrentBoard();
     const valid = isValidBoard(board);
 
     if (!valid) {
-      alert('Please remove invalid entries (highlighted in red) before attempting to solve.');
+      alert(
+        "Please remove invalid entries (highlighted in red) before attempting to solve."
+      );
       return;
     }
 
@@ -119,7 +125,7 @@ window.addEventListener('DOMContentLoaded', (e) => {
 
     // alert if no solution possible
     if (solution === null) {
-      alert('No solution is possible from the current board');
+      alert("No solution is possible from the current board");
       return;
     }
 
@@ -128,7 +134,7 @@ window.addEventListener('DOMContentLoaded', (e) => {
         const [val, pos] = el;
         const element = document.getElementById(String(pos + 1));
         if (val === 0) {
-          element.value = '';
+          element.value = "";
           element.parentElement.classList.remove("added");
         } else {
           element.parentElement.classList.remove("added");
@@ -140,5 +146,4 @@ window.addEventListener('DOMContentLoaded', (e) => {
       }, i * (10000 / path.length));
     });
   });
-
 });
